21fae6effcead11929688c067d5b94a96cfd5944
Peter Huynh <peterhyh@yahoo.com>
2023-08-18 20:00:02 -0500

    selenium finding and printing titles update

--- app.py
+++ app.py
@@ -6,17 +6,27 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-url = "https://peterhyh.com/"
+url = "https://selenium-python.readthedocs.io/locating-elements.html#locating-elements"
 
 driver = webdriver.Chrome()
 driver.get(url)
 
+titles = driver.find_elements(By.CLASS_NAME, 'section')
 
-try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'root'))
-    )
+list_of_titles = []
+
+for title in titles:
+    header = title.find_element(By.TAG_NAME, 'h2')
+    list_of_titles.append(header.text)
+
+
+print(list_of_titles)
+
+# try:
+#     main = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.ID, 'root'))
+#     )
     
-    print(main.text)
-finally:
-    driver.quit()
+#     print(main.text)
+# finally:
+#     driver.quit()
